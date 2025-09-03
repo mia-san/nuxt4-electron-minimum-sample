03853e68c65cbad3367d8ece0bbd682e4848453d
mia-san <mercenary_mia@hotmail.com>
2025-09-03 16:22:12 +0900

    bring 'quick-start' contents https://github.com/caoxiemeihao/nuxt-electron/blob/main/quick-start/

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
 </template>
+
+<script setup>
+import { onMounted } from 'vue'
+
+onMounted(async () => {
+  const time = await window.ipcRenderer.invoke('app-start-time')
+  console.log('App start time:', time)
+})
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,19 @@ export default defineNuxtConfig({
         // Main-Process entry file of the Electron App.
         entry: 'electron/main.ts',
       },
+      {
+        entry: 'electron/preload.ts',
+        onstart(args) {
+          // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete, 
+          // instead of restarting the entire Electron App.
+          args.reload()
+        },
+      },
     ],
+    // Ployfill the Electron and Node.js API for Renderer process.
+    // If you want use Node.js in Renderer process, the `nodeIntegration` needs to be enabled in the Main process.
+    // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
+    renderer: {},
   },
   ssr: false, // #43
 })
