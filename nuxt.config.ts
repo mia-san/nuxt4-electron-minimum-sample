c3959dd2158cd9ad5099eba45593e55cab17f188
mia-san <mercenary_mia@hotmail.com>
2025-09-03 16:11:19 +0900

    right after 'Quick Setup' finished https://github.com/caoxiemeihao/nuxt-electron#quick-setup
    note that I have downgraded `electron` from 38.0.0 to 37.4.0 purposely, otherwise build fails as follows:
    Could not detect abi for version 38.0.0 and runtime electron.  Updating "node-abi" might help solve this issue if it is a new release of electron  failedTask=build stackTrace=Error: Could not detect abi for version 38.0.0 and runtime electron.  Updating "node-abi" might help solve this issue if it is a new release of electron.
    
    latest node-abi@4.13.0 might work, but I don't want to 'manually' resolve package versions.
    also 37.4.0 is known to work, where as 38.0.0 is not.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['nuxt-electron'],
+  //  https://github.com/caoxiemeihao/nuxt-electron#quick-setup
+  electron: {
+    build: [
+      {
+        // Main-Process entry file of the Electron App.
+        entry: 'electron/main.ts',
+      },
+    ],
+  },
+  ssr: false, // #43
 })
